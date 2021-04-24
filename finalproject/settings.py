f7ee524e22f27969ddf388f88459d7bd3814cdc6
Beatrix <beatrixwho@gmail.com>
2021-04-24 02:39:55 -0700

    base app completed. add profile model. Add navbar. Add auth routes.

--- finalproject/settings.py
+++ finalproject/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'typeFiction',
 ]
 
 MIDDLEWARE = [
@@ -122,3 +123,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# URL
+LOGOUT_REDIRECT_URL = '/'
--- typeFiction/models.py
+++ typeFiction/models.py
@@ -1,3 +1,23 @@
+from django.contrib.auth.models import User
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
-# Create your models here.
+
+# Profile
+class Profile(models.Model):
+    username = models.OneToOneField(User, on_delete=models.CASCADE)
+    displayname = models.CharField(max_length=25)
+    image = models.CharField(max_length=250)
+    following = models.ManyToManyField(User, symmetrical=False, blank=True, related_name="follower")
+
+
+    def __str__(self):
+        return self.user.username
+
+# Create Profile when User is created
+@receiver(post_save, sender=User)
+def create_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+    instance.profile.save()
