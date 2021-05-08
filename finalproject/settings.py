ac7f86648bb51bc5677c29768a92705f9bb3f57a
Beatrix Woo <beatrixwho@gmail.com>
2021-05-07 18:51:01 -0700

    static files moved

--- finalproject/settings.py
+++ finalproject/settings.py
@@ -136,14 +136,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
-
-STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
-)
-
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # URL
